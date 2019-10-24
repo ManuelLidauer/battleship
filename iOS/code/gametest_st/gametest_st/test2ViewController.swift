8ca9ba100a5a093e709fdcb65663425ad9e0e47d
Stockinger Simon <stockinger.sim2@iMac05.local>
2019-10-24 15:18:31 +0200

    24102019 - seque fix

---
+++ iOS/code/gametest_st/gametest_st/test2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  test2ViewController.swift
+//  gametest_st
+//
+//  Created by Stockinger Simon on 24.10.19.
+//  Copyright © 2019 Stockinger Simon. All rights reserved.
+//
+
+import UIKit
+
+class test2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/code/guitest_st/guitest_st/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guitest_st
+//
+//  Created by Stockinger Simon on 17.10.19.
+//  Copyright © 2019 Stockinger Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
