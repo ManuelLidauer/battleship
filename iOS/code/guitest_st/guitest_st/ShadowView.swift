7217584c6de35eccd1e20914634fa64f3fee5e87
Lidauer Manuel <lidauer.man1@iMac04.local>
2019-11-21 17:00:26 +0100

    projekte zusammen geführt

---
+++ iOS/code/guitest_st/guitest_st/ShadowView.swift
@@ -0,0 +1,51 @@
+//
+//  ShadowView.swift
+//  guitest_st
+//
+//  Created by Lidauer Manuel on 21.11.19.
+//  Copyright © 2019 Stockinger Simon. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    //Shadow
+    @IBInspectable var shadowColor: UIColor = UIColor.black {
+        didSet {
+            self.updateView()
+        }
+    }
+    @IBInspectable var shadowOpacity: Float = 0.5 {
+        didSet {
+            self.updateView()
+        }
+    }
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 3, height: 3) {
+        didSet {
+            self.updateView()
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat = 15.0 {
+        didSet {
+            self.updateView()
+        }
+    }
+
+    //Apply params
+    func updateView() {
+        self.layer.shadowColor = self.shadowColor.cgColor
+        self.layer.shadowOpacity = self.shadowOpacity
+        self.layer.shadowOffset = self.shadowOffset
+        self.layer.shadowRadius = self.shadowRadius
+    }
+    
+}
