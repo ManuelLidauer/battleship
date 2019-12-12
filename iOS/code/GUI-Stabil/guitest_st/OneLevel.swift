0f6c4373347a0cb4b2027a52184cee616789f8af
Lidauer Manuel <lidauer.man1@iMac15.local>
2019-12-12 16:59:04 +0100

    segues

---
+++ iOS/code/GUI-Stabil/guitest_st/OneLevel.swift
@@ -0,0 +1,16 @@
+//
+//  OneLevel.swift
+//  guitest_st
+//
+//  Created by Lidauer Manuel on 12.12.19.
+//  Copyright © 2019 Stockinger Simon. All rights reserved.
+//
+
+import Foundation
+
+class OneLevel {
+    var id: Int = 0
+    var name: String = ""
+    var pfad: String = ""
+}
+
