f37838437e6bd10e430c024880173a47ea5b1145
Stockinger Simon <stockinger.sim2@iMac05.local>
2019-12-19 14:15:05 +0100

    last commit

--- iOS/code/guitest_st/guitest_st/DesignableView.swift
+++ iOS/code/guitest_st/guitest_st/DesignableView.swift
@@ -37,18 +37,6 @@ import UIKit
     private func setGradient()
     {
         
-        /*
-         let layer = CAGradientLayer()
-         layer.locations = [0.0 , 1.0]
-         layer.startPoint = CGPoint(x: 0.5, y: 0.0)
-         layer.endPoint = CGPoint(x: 0.5, y: 1.0)
-         layer.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
-         
-         layer.colors = [CGColor.init(srgbRed: 0.4, green: 0.2, blue: 0.4, alpha: 1),
-                         CGColor.init(srgbRed: 0.5, green: 0.7, blue: 0.4, alpha: 1)]
-         view.layer.insertSublayer(layer, at: 0)
-         */
-        
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
         gradientLayer.startPoint = self.gradientStartPoint
